2c9d7eda8da3d6c36cf9986d50ef4462f814d1d1
tusherbhakta <tusheranta@gmail.com>
2025-03-21 01:40:11 +0530

    feat: enhance ProductCard with currency icon and style updates

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -3,6 +3,8 @@ import React, { useContext } from "react";
 import { useNavigate } from "react-router";
 import { AuthContext } from "../../provider/AuthProvider";
 import { FiArrowRight } from "react-icons/fi"; // Import arrow icon
+import { FaBangladeshiTakaSign } from "react-icons/fa6";
+
 
 const ProductCard = ({ product }) => {
   const { setProductId } = useContext(AuthContext);
@@ -23,17 +25,17 @@ const ProductCard = ({ product }) => {
       <div className="p-5">
         <h2 className="text-2xl font-semibold">{product?.product_name}</h2>
         <p className="text-gray-500 text-lg">{product?.product_category}</p>
-        <h2 className="font-bold text-lg mt-2">{product?.product_price}.00 tk</h2>
+        <h2 className="font-bold text-lg mt-2 flex gap-1 items-center"><FaBangladeshiTakaSign />{product?.product_price}.00</h2>
 
         {/* View Details Button */}
         <button
-          className="mt-5 flex items-center text-primary cursor-pointer font-semibold hover:text-blue-700 transition-all duration-300"
+          className="mt-5 flex items-center  cursor-pointer font-semibold bg-gradient-to-r from-[#e63946] via-[#6a0572] bg-clip-text text-transparent transition-all duration-300"
           onClick={() => {
             navigate(`/product-details/${product?.product_id}`);
             setProductId(product?.product_id);
           }}
         >
-          View Details<FiArrowRight className="ml-2 text-xl" />
+          View Details<FiArrowRight className="ml-2 text-xl text-[#6a0572]" />
         </button>
       </div>
     </div>
