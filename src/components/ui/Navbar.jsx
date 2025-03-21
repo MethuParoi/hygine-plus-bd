2c4e1f392a3c4b39b3959df81f976161e49c2f7e
MethuParoi <paroimethu@gmail.com>
2025-03-21 23:51:00 +0530

    feat: enhance Navbar with dropdown menus for Bathware and Kitchenware

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,13 +1,14 @@
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { FaSearch } from "react-icons/fa";
 import flag from "../../assets/navbar/bd-flag.png";
 import logo from "../../assets/logo/logo-white.png";
+import { IoIosArrowDropdown } from "react-icons/io";
 
 const Navbar = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="bg-gray-900 text-gray-50 shadow-sm pt-4 px-8">
+    <div className="bg-black text-gray-50 shadow-sm pt-4 px-12">
       {/* upper navbar */}
       <div className="flex justify-between items-center mb-2">
         <div>
@@ -23,9 +24,9 @@ const Navbar = () => {
               />
               <button
                 onClick={() => navigate("/")}
-                className="absolute top-0 right-0 text-lg cursor-pointer bg-gray-600 hover:bg-gray-700 h-10 w-12 flex items-center justify-center rounded-r-[2rem] focus:ring-0 focus:ring-transparent"
+                className="absolute top-0 right-0 text-lg cursor-pointer bg-transparent h-10 w-12 flex items-center justify-center rounded-r-[2rem] focus:ring-0 focus:ring-transparent"
               >
-                <FaSearch className="text-2xl" />
+                <FaSearch className="text-2xl text-black" />
               </button>
             </div>
             <button
@@ -50,19 +51,51 @@ const Navbar = () => {
 
       {/* lower navbar */}
       <div className="border-t border-gray-50 mt-2 flex justify-between items-center">
-        <div className="flex items-center gap-x-4">
+        <div className="flex items-center gap-x-4 relative">
           <button
-            onClick={() => navigate("/")}
-            className="text-lg cursor-pointer font-semibold hover:text-gray-300"
+            popoverTarget="bathware"
+            style={{ anchorName: "--anchor-1" }}
+            className="text-lg cursor-pointer font-semibold hover:text-gray-300 flex items-center gap-x-1"
           >
             Bathware
+            <IoIosArrowDropdown className="text-2xl" />
           </button>
+          {/* bathware dropdown */}
+          <ul
+            className="dropdown menu w-52 rounded-box bg-gray-800 shadow-sm text-white"
+            popover="auto"
+            id="bathware"
+            style={{ positionAnchor: "--anchor-1" }}
+          >
+            <li className="hover:bg-gray-600">
+              <Link>bathware</Link>
+            </li>
+            <li className="hover:bg-gray-600">
+              <Link>Item 2</Link>
+            </li>
+          </ul>
           <button
-            onClick={() => navigate("/")}
-            className="text-lg cursor-pointer font-semibold hover:text-gray-300"
+            popoverTarget="kitchenware"
+            style={{ anchorName: "--anchor-2" }}
+            className="text-lg cursor-pointer font-semibold hover:text-gray-300 flex items-center gap-x-1"
           >
             Kitchenware
+            <IoIosArrowDropdown className="text-2xl" />
           </button>
+          {/* kitchenware dropdown */}
+          <ul
+            className="dropdown menu w-52 rounded-box bg-gray-800 shadow-sm text-white"
+            popover="auto"
+            id="kitchenware"
+            style={{ positionAnchor: "--anchor-2" }}
+          >
+            <li className="hover:bg-gray-600">
+              <Link>Kitchenware</Link>
+            </li>
+            <li className="hover:bg-gray-600">
+              <Link>Item 2</Link>
+            </li>
+          </ul>
         </div>
 
         {/* logo */}
@@ -81,7 +114,7 @@ const Navbar = () => {
             onClick={() => navigate("/")}
             className="text-lg cursor-pointer font-semibold hover:text-gray-300"
           >
-            Careers
+            About Us
           </button>
         </div>
       </div>
