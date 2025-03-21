bae3260aef6af3e17096a156c197551cca23c601
tusherbhakta <tusheranta@gmail.com>
2025-03-21 23:27:18 +0530

    add FactoryImg component and update NewCollection with product images

---
+++ src/components/home/FactoryImg.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FactoryImg = () => {
+    return (
+        <div className='w-11/12 lg:w-10/12 mx-auto my-20'>
+            
+            
+        </div>
+    );
+};
+
+export default FactoryImg;
--- src/components/home/NewCollection.jsx
+++ src/components/home/NewCollection.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { motion } from "framer-motion";
+import faucet2 from '../../assets/products/faucet2.jpg'
+import shower from '../../assets/products/Shower-panel3.jpg'
+import floorDrain from '../../assets/products/Floor-darin2.jpg'
 
 const NewCollection = () => {
     return (
-        <div className='w-11/12 mx-auto my-20'>
+        <div className='w-11/12 lg:w-10/12 mx-auto my-20'>
             {/* Title with animation (from bottom) */}
             <motion.div
                 initial={{ y: 100, opacity: 0 }}
@@ -17,15 +20,24 @@ const NewCollection = () => {
                     Beyond inspiration, into excellence
                 </p>
             </motion.div>
-            <motion.div
-            
-                initial={{ x: -300, opacity: 0 }}
-                whileInView={{ x: 0, opacity: 1 }}
-                viewport={{ once: true }}
-                transition={{ duration: 2, ease: "easeOut" }}
-                className="gird grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center">
-                    <img src={} alt="" />
-            </motion.div>
+            <motion.dev 
+            initial={{ y: 300, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 4, ease: "easeOut" }}
+             >
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-items-center my-10'>
+                <div>
+                    <img src={faucet2} alt="faucet Image" />
+                </div>
+                <div>
+                    <img src={shower} alt="Shower Image" />
+                </div>
+                <div>
+                    <img src={floorDrain} alt="Floor Drain Image" />
+                </div>
+            </div>
+            </motion.dev>
         </div>
     );
 };
