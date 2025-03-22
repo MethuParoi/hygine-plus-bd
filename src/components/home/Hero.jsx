4a885118ba5bfb004e1c67aa80a22f585105a4b6
MethuParoi <paroimethu@gmail.com>
2025-03-22 21:32:09 +0530

    feat: implement Hero component with scroll effect and background image

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,5 +1,68 @@
+import { useEffect, useState } from "react";
+import bgImage from "../../assets/hero/hero.jpg";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Check if the scroll position is greater than 100px (adjust as needed)
+      if (window.scrollY > 300) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <div
+      className="h-[85dvh] w-full max-w-full overflow-hidden grid grid-cols-6 "
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      {/* first column */}
+      <div className="col-span-2 flex items-center justify-center">
+        <h1
+          className={`text-black text-6xl font-extrabold mb-36 ${
+            isScrolled ? "" : "fixed"
+          }`}
+        >
+          ENVISION
+        </h1>
+      </div>
+      {/* second column */}
+      <div className=" flex items-center justify-center">
+        <h1
+          className={`text-white text-6xl font-extrabold ml-20 mb-36 ${
+            isScrolled ? "" : "fixed"
+          }`}
+        >
+          ELEVATE
+        </h1>
+      </div>
+      {/* third column */}
+      <div className="col-span-3 flex items-center justify-center">
+        <h1
+          className={`text-red-600 text-6xl font-extrabold mb-36 ml-36 ${
+            isScrolled ? "" : "fixed"
+          }`}
+        >
+          EXPERIENCE
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import FactoryImg from "../components/home/FactoryImg";
 import VideoSection from "../components/home/VideoSection";
-import Hero from "../components/home/VideoSection";
 import LastMonthStart from "../components/home/LastMonthStart";
 import NewArrival from "../components/home/NewArrival";
 import NewCollection from "../components/home/NewCollection";
 import PartnersSection from "../components/home/OurPartners";
 import ShopByCatagory from "../components/home/ShopByCatagory";
 import { getProducts } from "../utils/apiProduct";
+import Hero from "../components/home/Hero";
 
 const Home = () => {
   // Fetch products from the database once in a day 86400000
@@ -15,6 +15,7 @@ const Home = () => {
   }, 10000);
   return (
     <div className="overflow-x-hidden">
+      <Hero></Hero>
       <ShopByCatagory></ShopByCatagory>
       <VideoSection></VideoSection>
       <NewArrival></NewArrival>
