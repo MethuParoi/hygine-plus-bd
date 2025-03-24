3f81b725e66e64e149cb06c240f02fa0aa9a8c4b
MethuParoi <paroimethu@gmail.com>
2025-03-25 02:16:45 +0530

    feat: enhance SortButton and SortProduct components for improved category sorting functionality

--- src/components/products/SortProduct.jsx
+++ src/components/products/SortProduct.jsx
@@ -1,14 +1,10 @@
-import SortIndividual from "./SortIndividual";
 import { getProducts } from "../../utils/apiProduct";
 import SortButton from "./SortButton";
 import { useEffect, useState } from "react";
 
 function SortProduct() {
   const [categories, setCategories] = useState(new Set());
-
-  //   const selectedCategories = useSelector(
-  //     (state) => state.sortProduct.selectedOptions
-  //   );
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   //fetch categories
   async function fetchCategory() {
@@ -18,20 +14,31 @@ function SortProduct() {
 
   useEffect(() => {
     fetchCategory();
-    console.log("categories:", categories);
   }, []);
 
   // Convert categories to an array of Option objects
   const categoryOptions = Array.from(categories).map((category) => ({
-    label: category,
+    label: category.charAt(0).toUpperCase() + category.slice(1), //
     value: category,
   }));
 
+  const handleCategoryChange = (value) => {
+    setSelectedCategory(value); // Update the selected category
+    console.log("Selected category:", value);
+  };
+
   return (
-    <div>
-      <h1>Sort Products</h1>
-      <div className="xl:border-t-2 border-gray-300 mr-[2rem] pt-[1rem]">
-        <SortButton label={"Sort by Category"} options={categories} />
+    <div className="mt-10">
+      <h1 className="text-3xl font-semibold text-gray-600">Sort Products</h1>
+      <div className="xl:border-t-2 border-gray-300 mr-[2rem] pt-[2rem] flex flex-col gap-y-6 justify-center items-center">
+        {categoryOptions.map((category, index) => (
+          <SortButton
+            key={index}
+            options={category}
+            isSelected={selectedCategory === category.value} // Pass selected state
+            onChange={handleCategoryChange}
+          />
+        ))}
       </div>
     </div>
   );
