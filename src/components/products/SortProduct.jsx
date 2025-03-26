11f6b509366bef7610437d0de44a4c91f303cb6f
MethuParoi <paroimethu@gmail.com>
2025-03-26 15:49:28 +0530

    feat: enhance SortButton component with icon and adjust layout in SortProduct for better alignment

--- src/components/products/SortProduct.jsx
+++ src/components/products/SortProduct.jsx
@@ -37,7 +37,7 @@ function SortProduct() {
   return (
     <div className="mt-10">
       <h1 className="text-3xl font-semibold text-gray-600">Sort Products</h1>
-      <div className="xl:border-t-2 border-gray-300 md:mr-[2rem] pt-[2rem] flex flex-row gap-x-2 flex-wrap lg:gap-x-0 lg:flex-col gap-y-2 lg:gap-y-6 justify-center items-center">
+      <div className="xl:border-t-2 border-gray-300 md:mr-[2rem] pt-[2rem] flex flex-row gap-x-2 flex-wrap lg:gap-x-0 lg:flex-col gap-y-2 lg:gap-y-0 justify-center items-center">
         {categoryOptions.map((category, index) => (
           <SortButton
             key={index}
