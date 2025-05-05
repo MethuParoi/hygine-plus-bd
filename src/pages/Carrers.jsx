a6b05a74d31031db30f737a469697cbbe0449690
tusherbhakta <tusheranta@gmail.com>
2025-05-05 09:40:17 +0530

    carrers video added

--- src/pages/Carrers.jsx
+++ src/pages/Carrers.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import carrersVideo from "../assets/Carrers/career 1st .mp4"
 
 const Carrers = () => {
   return (
-    <div>Carrers</div>
+    <div>
+      {/* video section */}
+      <div>
+        <video
+          className="w-full max-h-screen object-cover"
+          src={carrersVideo} autoPlay
+          loop
+          muted
+          playsInline ></video>
+      </div>
+    </div>
   )
 }
 
